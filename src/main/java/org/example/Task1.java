1fcf2fe6acfda8eb7d818923af57b2cf495f74e7
keizhun <ethankeith876@gmail.com>
2025-01-27 13:18:55 +0800

    Initial commit

---
+++ src/main/java/org/example/Task1.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.println("Please enter second number: ");
+        int num2 = scanner.nextInt();
+
+        double result;
+
+        result = num1 + num2;
+        System.out.println("Addition: " + result);
+
+        result = num1 - num2;
+        System.out.println("Subtraction: " + result);
+
+        result = num1 * num2;
+        System.out.println("Multiplication: " + result);
+
+        result = (double) num1 / num2;
+        System.out.println("Division: " + result);
+    }
+}
+
---
+++ src/main/java/org/example/Task2.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        String name = "Keith";
+        int age = 18;
+        double height = 1.6;
+        boolean isStudent = true;
+
+        System.out.println("Hi, my name is " + name + ". I am " + age + " years old, " + height + " meters tall, and it is " + isStudent + " that I am a student.");
+
+
+
+
+    }
+}
